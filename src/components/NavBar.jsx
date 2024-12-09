0147f2b601691fb88f0d589a8ce86f1b59fd6b29
MohaHuzai <muhammad.huzaima@uni.eus>
2024-12-09 13:31:24 +0100

    NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,7 +16,7 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">FadGPT</a>
+        <a className="btn btn-ghost text-xl">HuzaiGPT</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
